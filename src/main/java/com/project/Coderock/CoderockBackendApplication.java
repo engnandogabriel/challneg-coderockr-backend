6735714f4f3d5f55b58abb7df04deb19275f51ec
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-11 14:38:59 -0300

    first commit

---
+++ src/main/java/com/project/Coderock/CoderockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.Coderock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderockBackendApplication.class, args);
+	}
+
+}
