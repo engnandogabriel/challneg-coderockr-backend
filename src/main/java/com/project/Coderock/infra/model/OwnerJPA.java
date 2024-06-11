422ea1b988305375f4213e3844a05edfcdf3eb04
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-11 15:33:39 -0300

    feat: added getByEmail in OwnerJPA

--- src/main/java/com/project/Coderock/infra/model/OwnerJPA.java
+++ src/main/java/com/project/Coderock/infra/model/OwnerJPA.java
@@ -2,6 +2,9 @@ package com.project.Coderock.infra.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OwnerJPA extends JpaRepository<OwnerModel, String> {
 
+    Optional<OwnerModel> findByEmail(String email);
 }
