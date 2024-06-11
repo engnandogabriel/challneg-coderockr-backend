dac6d716b237d4fee6f940ac47c5b7de984378f7
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-11 15:33:20 -0300

    feat: added getByEmail

--- src/main/java/com/project/Coderock/application/repository/OwnerRepository.java
+++ src/main/java/com/project/Coderock/application/repository/OwnerRepository.java
@@ -1,7 +1,11 @@
 package com.project.Coderock.application.repository;
 
 import com.project.Coderock.domain.entites.Owner;
+import com.project.Coderock.infra.model.OwnerJPA;
+
+import java.util.Optional;
 
 public interface OwnerRepository {
     void save(Owner owner);
+    Optional<Owner> getByEmail(String email);
 }
