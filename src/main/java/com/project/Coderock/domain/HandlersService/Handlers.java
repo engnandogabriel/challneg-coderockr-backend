3f69d092b003c6170880ddbf667d381397b20c08
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-11 15:37:32 -0300

    feat: added Handlers costumiezed for app response

---
+++ src/main/java/com/project/Coderock/domain/HandlersService/Handlers.java
@@ -0,0 +1,17 @@
+package com.project.Coderock.domain.HandlersService;
+
+import org.springframework.http.HttpStatus;
+
+public class Handlers<T> {
+    public HandlerDTO badRquest(Exception e) {
+        return new HandlerDTO<String>(HttpStatus.BAD_REQUEST, "Bad Request", e.getMessage());
+    }
+
+    public HandlerDTO servrError(Exception e) {
+        return new HandlerDTO<String>(HttpStatus.INTERNAL_SERVER_ERROR, "ServerError", "Internal Server Error: Please, try again later");
+    }
+
+    public HandlerDTO success(T obj) {
+        return new HandlerDTO<T>(HttpStatus.OK, "Success", obj);
+    }
+}
