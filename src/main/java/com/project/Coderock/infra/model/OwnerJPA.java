412fc1805dab66878457b5c3920f6d9458630c6b
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-11 15:21:08 -0300

    feat: added OwnerJPA to mapping data base

---
+++ src/main/java/com/project/Coderock/infra/model/OwnerJPA.java
@@ -0,0 +1,7 @@
+package com.project.Coderock.infra.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerJPA extends JpaRepository<OwnerModel, String> {
+
+}
