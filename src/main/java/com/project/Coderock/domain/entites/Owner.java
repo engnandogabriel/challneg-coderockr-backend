5b9a4838a3907423f706b19e011cb5143e123a7f
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-11 15:15:24 -0300

    feat: added Email in Owner

--- src/main/java/com/project/Coderock/domain/entites/Owner.java
+++ src/main/java/com/project/Coderock/domain/entites/Owner.java
@@ -1,26 +1,30 @@
 package com.project.Coderock.domain.entites;
 
+import com.project.Coderock.domain.value_objects.Email;
+
 import java.util.UUID;
 
 public class Owner {
     private String owner_id;
     private String name;
+    private Email email;
 
     public Owner() {
     }
 
-    private Owner(String owner_id, String name) {
+    private Owner(String owner_id, String name, Email email) {
         this.owner_id = owner_id;
         this.name = name;
+        this.email = email;
     }
 
-    public static Owner create(String name) {
+    public static Owner create(String name, String email) throws Exception{
         UUID id = UUID.randomUUID();
-        return new Owner(id.toString(), name);
+        return new Owner(id.toString(), name, Email.create(email));
     }
 
-    public static Owner restore(String id, String name) {
-        return new Owner(id, name);
+    public static Owner restore(String id, String name,String email) {
+        return new Owner(id, name, Email.restore(email));
     }
 
     public String getOwner_id() {
@@ -37,5 +41,9 @@ public class Owner {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email.getEmail();
     }
 }
