ca165c837cf0baaf881b21892960a3704e56ca61
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-11 15:38:59 -0300

    feat: added Handlers in OwnerController

--- src/main/java/com/project/Coderock/controller/OwnerController.java
+++ src/main/java/com/project/Coderock/controller/OwnerController.java
@@ -2,6 +2,7 @@ package com.project.Coderock.controller;
 
 import com.project.Coderock.application.usecases.CreateOwner;
 import com.project.Coderock.domain.DTO.CreateOwnerDTO;
+import com.project.Coderock.domain.HandlersService.HandlerDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,8 +18,8 @@ public class OwnerController {
     }
 
     @PostMapping("")
-    public ResponseEntity<String> save(@RequestBody  CreateOwnerDTO ownerDTO){
-        this.createOwner.execute(ownerDTO);
-        return ResponseEntity.ok("Created");
+    public ResponseEntity<HandlerDTO> save(@RequestBody  CreateOwnerDTO ownerDTO){
+        HandlerDTO output = this.createOwner.execute(ownerDTO);
+        return new ResponseEntity<>(output, output.status());
     }
 }
