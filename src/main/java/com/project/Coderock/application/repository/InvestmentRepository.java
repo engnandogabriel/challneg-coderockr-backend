b10a5e8b1e2173f9da98039ad7373088f20195f6
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-12 12:58:52 -0300

    feat: added InvestmentRepository

---
+++ src/main/java/com/project/Coderock/application/repository/InvestmentRepository.java
@@ -0,0 +1,7 @@
+package com.project.Coderock.application.repository;
+
+import com.project.Coderock.domain.entites.Investment;
+
+public interface InvestmentRepository {
+    void save(Investment investment);
+}
