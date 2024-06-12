ffa3f07be356662284af7c2f2c3279b7b8d23a7d
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-12 12:59:17 -0300

    feat: added InvestmentJPA

---
+++ src/main/java/com/project/Coderock/infra/model/InvestmentJPA.java
@@ -0,0 +1,6 @@
+package com.project.Coderock.infra.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvestmentJPA extends JpaRepository<InvestmentModel, String> {
+}
