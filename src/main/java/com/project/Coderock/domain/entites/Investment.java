46e81c237f2684d18cb2ab29e773966468d467c6
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-12 14:03:21 -0300

    feat: added investment propriety

--- src/main/java/com/project/Coderock/domain/entites/Investment.java
+++ src/main/java/com/project/Coderock/domain/entites/Investment.java
@@ -10,29 +10,31 @@ public class Investment {
     private String investment_id;
     private String owner_id;
     private CreateDate create_date;
+    private Double investment;
     private Double amount;
     private Status status;
 
     public Investment() {
     }
 
-    private Investment(String investment_id, String owner_id, CreateDate create_date, Double amount, Status status) {
+    private Investment(String investment_id, String owner_id, CreateDate create_date, Double investment, Double amount, Status status) {
         this.investment_id = investment_id;
         this.owner_id = owner_id;
         this.create_date = create_date;
         this.amount = amount;
+        this.investment = investment;
         this.status = status;
     }
 
-    public static Investment create(String owner_id, String create_date, Double amount) throws Exception {
+    public static Investment create(String owner_id, String create_date, Double investment) throws Exception {
         UUID id = UUID.randomUUID();
         String status = "in_investment";
-        if(amount.doubleValue() <= 0) throw new InvalidParamError("Invalid Amount");
-        return new Investment(id.toString(), owner_id, CreateDate.validateDate(create_date), amount, Status.create());
+        if(investment.doubleValue() <= 0) throw new InvalidParamError("Invalid Amount");
+        return new Investment(id.toString(), owner_id, CreateDate.validateDate(create_date), investment, 0.0, Status.create());
     }
 
-    public static Investment restore(String id, String owner_id, String create_date, Double amount, String status) throws Exception {
-        return new Investment(id, owner_id, CreateDate.restore(create_date), amount, Status.restore(status));
+    public static Investment restore(String id, String owner_id, String create_date, Double investment, Double amount, String status) throws Exception {
+        return new Investment(id, owner_id, CreateDate.restore(create_date), investment, amount, Status.restore(status));
     }
 
     public String getInvestment_id() {
@@ -48,6 +50,14 @@ public class Investment {
         return create_date.getCreate_date();
     }
 
+    public Double getInvestment() {
+        return investment;
+    }
+
+    public void setInvestment(Double investment) {
+        this.investment = investment;
+    }
+
     public Double getAmount() {
         return amount;
     }
@@ -61,5 +71,8 @@ public class Investment {
     }
     public void setWithdrawn() throws Exception{
         this.status.setWithdrawn();
+    }
+    public void getBalance(){
+
     }
 }
