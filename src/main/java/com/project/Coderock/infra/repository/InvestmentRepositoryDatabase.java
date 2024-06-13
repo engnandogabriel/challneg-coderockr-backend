d44e7acebe9058f5f4191d2f88e60dd957fe20f4
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-13 14:33:46 -0300

    feat: added getById in InvestmentRepositoryDatabase

--- src/main/java/com/project/Coderock/infra/repository/InvestmentRepositoryDatabase.java
+++ src/main/java/com/project/Coderock/infra/repository/InvestmentRepositoryDatabase.java
@@ -6,6 +6,8 @@ import com.project.Coderock.infra.model.InvestmentJPA;
 import com.project.Coderock.infra.model.InvestmentModel;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public class InvestmentRepositoryDatabase implements InvestmentRepository {
     private InvestmentJPA investmentJPA;
@@ -18,5 +20,13 @@ public class InvestmentRepositoryDatabase implements InvestmentRepository {
     public void save(Investment investment) {
         InvestmentModel investmentModel = new InvestmentModel(investment.getInvestment_id(), investment.getOwner_id(), investment.getCreate_date(), investment.getInvestment(), investment.getAmount(), investment.getStatus());
         this.investmentJPA.save(investmentModel);
+    }
+
+    @Override
+    public Optional<Investment> getById(String investment_id) throws Exception {
+        InvestmentModel investmentModel = this.investmentJPA.findById(investment_id).orElse(null);
+        if (investment_id == null) return Optional.empty();
+        Investment investment = Investment.restore(investmentModel.getInvestment_id(), investmentModel.getOwner_id(), investmentModel.getCreate_date(), investmentModel.getInvestment(), investmentModel.getAmount(), investmentModel.getStatus());
+        return Optional.of(investment);
     }
 }
