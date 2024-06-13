d554755282bc72d2950a560784aca2bb6739ef9f
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-13 14:38:29 -0300

    feat: added ViewInvestment in InvestmentController

--- src/main/java/com/project/Coderock/controller/InvestmentController.java
+++ src/main/java/com/project/Coderock/controller/InvestmentController.java
@@ -1,26 +1,30 @@
 package com.project.Coderock.controller;
 
 import com.project.Coderock.application.usecases.CreateInvestment;
+import com.project.Coderock.application.usecases.ViewInvestment;
 import com.project.Coderock.domain.DTO.CreateInvestmentDTO;
 import com.project.Coderock.domain.HandlersService.HandlerDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("investment")
 public class InvestmentController {
     private CreateInvestment createInvestment;
-
-    public InvestmentController(CreateInvestment createInvestment){
+    private ViewInvestment viewInvestment;
+    public InvestmentController(CreateInvestment createInvestment,ViewInvestment viewInvestment){
         this.createInvestment = createInvestment;
+        this.viewInvestment = viewInvestment;
     }
 
     @PostMapping
     public ResponseEntity<HandlerDTO> save(@RequestBody CreateInvestmentDTO createInvestmentDTO){
         HandlerDTO output = this.createInvestment.execute(createInvestmentDTO);
         return new ResponseEntity<>(output, output.status());
+    }
+    @GetMapping(value = "{id}/view")
+    public ResponseEntity<HandlerDTO> view(@PathVariable String id){
+        HandlerDTO output = this.viewInvestment.execute(id);
+        return  new ResponseEntity<>(output, output.status());
     }
 }
