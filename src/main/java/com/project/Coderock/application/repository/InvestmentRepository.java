8d4b85eebb033fe54c31083577d8a9c2737c98ff
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-13 14:33:10 -0300

    feat: added getByid in InvestmentRepository

--- src/main/java/com/project/Coderock/application/repository/InvestmentRepository.java
+++ src/main/java/com/project/Coderock/application/repository/InvestmentRepository.java
@@ -2,6 +2,9 @@ package com.project.Coderock.application.repository;
 
 import com.project.Coderock.domain.entites.Investment;
 
+import java.util.Optional;
+
 public interface InvestmentRepository {
     void save(Investment investment);
+    Optional<Investment> getById(String investment_id) throws Exception;
 }
