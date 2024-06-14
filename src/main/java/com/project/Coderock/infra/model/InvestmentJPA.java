3ba4ab9e10bda01567176c9952d8a2c9a1d3bcf8
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-14 15:59:01 -0300

    feat: added findByOwnerId method

--- src/main/java/com/project/Coderock/infra/model/InvestmentJPA.java
+++ src/main/java/com/project/Coderock/infra/model/InvestmentJPA.java
@@ -1,6 +1,9 @@
 package com.project.Coderock.infra.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvestmentJPA extends JpaRepository<InvestmentModel, String> {
+    Page<InvestmentModel> findByOwnerId(String ownerId, Pageable pageable);
 }
