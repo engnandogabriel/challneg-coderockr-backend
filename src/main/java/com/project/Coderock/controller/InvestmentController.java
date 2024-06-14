955779fb346c0ea2eef5348216573dd1ee634472
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-14 16:00:28 -0300

    feat: added GetOwnerInvestments in InvestmentController

--- src/main/java/com/project/Coderock/controller/InvestmentController.java
+++ src/main/java/com/project/Coderock/controller/InvestmentController.java
@@ -1,6 +1,7 @@
 package com.project.Coderock.controller;
 
 import com.project.Coderock.application.usecases.CreateInvestment;
+import com.project.Coderock.application.usecases.GetOwnerInvestments;
 import com.project.Coderock.application.usecases.ViewInvestment;
 import com.project.Coderock.application.usecases.WithdrawalInvestment;
 import com.project.Coderock.domain.DTO.CreateInvestmentDTO;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.*;
 public class InvestmentController {
     private CreateInvestment createInvestment;
     private ViewInvestment viewInvestment;
-    protected WithdrawalInvestment withdrawalInvestment;
+    private WithdrawalInvestment withdrawalInvestment;
+    private GetOwnerInvestments getOwnerInvestments;
 
-    public InvestmentController(CreateInvestment createInvestment, ViewInvestment viewInvestment, WithdrawalInvestment withdrawalInvestment) {
+    public InvestmentController(CreateInvestment createInvestment, ViewInvestment viewInvestment, WithdrawalInvestment withdrawalInvestment, GetOwnerInvestments getOwnerInvestments) {
         this.createInvestment = createInvestment;
         this.viewInvestment = viewInvestment;
         this.withdrawalInvestment = withdrawalInvestment;
+        this.getOwnerInvestments = getOwnerInvestments;
     }
 
     @PostMapping
@@ -37,6 +40,12 @@ public class InvestmentController {
     @PostMapping(value = "{id}/withdrawal")
     public ResponseEntity<HandlerDTO> withdrawal(@PathVariable String id, @RequestBody WithdrawalInvestimentDTO withdrawalInvestimentDTO) {
         HandlerDTO output = this.withdrawalInvestment.exceute(id, withdrawalInvestimentDTO.withdrawal_date());
+        return new ResponseEntity<>(output, output.status());
+    }
+
+    @GetMapping(value = "{id}/page")
+    public ResponseEntity<HandlerDTO> page(@PathVariable String id, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size) {
+        HandlerDTO output = this.getOwnerInvestments.execute(id, page, size);
         return new ResponseEntity<>(output, output.status());
     }
 }
