c6a2f4da2d1e04822be1afaf08bd055386365067
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-29 11:23:43 -0300

    feat: LocalDate in Balance

--- src/main/java/com/project/Coderock/domain/entites/Balance/Balance.java
+++ src/main/java/com/project/Coderock/domain/entites/Balance/Balance.java
@@ -1,13 +1,15 @@
 package com.project.Coderock.domain.entites.Balance;
 
+import java.time.LocalDate;
+
 public abstract class Balance {
     protected Double tax = 0.0052;
 
-    public abstract Double setBalance(Double amount, String create_date, String viewDate) throws Exception;
+    public abstract Double setBalance(Double amount, LocalDate create_date, String viewDate) throws Exception;
 
-    public abstract int getMonths(String create_date, String view_date) throws Exception;
+    public abstract int getMonths(LocalDate create_date, String view_date) throws Exception;
 
-    protected double getGains(String create_date, String viewDate) throws Exception{
+    protected double getGains(LocalDate create_date, String viewDate) throws Exception{
         int months = this.getMonths(create_date, viewDate);
         if (months <= 0) return 0.0;
         return Math.pow(1 + this.tax, months);
