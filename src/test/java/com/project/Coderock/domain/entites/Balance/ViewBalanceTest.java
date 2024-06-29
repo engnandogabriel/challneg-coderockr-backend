fab823846c7cd4e0fce7693cde5d8df1c395b098
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-29 11:54:00 -0300

    feat: added tests

--- src/test/java/com/project/Coderock/domain/entites/Balance/ViewBalanceTest.java
+++ src/test/java/com/project/Coderock/domain/entites/Balance/ViewBalanceTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 
 class ViewBalanceTest {
     @Test
     @DisplayName("Should be returned a correct value")
     void data() throws Exception {
-        Double view = new ViewBalance().setBalance(1000D, "10/01/2024", "");
+        LocalDate createDate = LocalDate.parse("2024-01-10");
+        Double view = new ViewBalance().setBalance(1000D, createDate, "");
         Assertions.assertEquals(1026.27, view);
     }
 }
