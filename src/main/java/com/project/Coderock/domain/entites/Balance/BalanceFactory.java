41dd22164a10f2f931c9c9faf0d0829498be3106
engnandogabriel <eng.nandogabriel@gmail.com>
2024-06-29 11:53:29 -0300

    feat: added LocalDate in BalanceFactory

--- src/main/java/com/project/Coderock/domain/entites/Balance/BalanceFactory.java
+++ src/main/java/com/project/Coderock/domain/entites/Balance/BalanceFactory.java
@@ -1,7 +1,9 @@
 package com.project.Coderock.domain.entites.Balance;
 
+import java.time.LocalDate;
+
 public class BalanceFactory {
-    public static Double balance(String type, Double investment, String create_date, String viewDate) throws Exception {
+    public static Double balance(String type, Double investment, LocalDate create_date, String viewDate) throws Exception {
         if (type.equals("view_balance")) return new ViewBalance().setBalance(investment, create_date, viewDate);
         if (type.equals("calculate")) return new WithdrawalBalance().setBalance(investment, create_date, viewDate);
         throw new Exception("Invalid type");
